9530d24b0ef94aa8574a54acd94f6168f35c3a18
Micah Bonzet <micah@farsoft.co.za>
2024-01-23 16:47:37 +0200

    Added ProjectsV2 controller
    V2 uses API key and the original doesn't

--- AtlaswayInternalAPI/Controllers/ProjectsController.cs
+++ AtlaswayInternalAPI/Controllers/ProjectsController.cs
@@ -7,12 +7,64 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AtlaswayInternalAPI.Controllers;
 
-[ApiKeyAuthFilter]
 public class ProjectsController : BaseController
 {
     #region Constructors
 
     public ProjectsController(AtlaswayInternalDbContext dbContext) : base(dbContext) { }
+
+    #endregion
+
+    #region Network Requests
+
+    //GET: api/Projects
+    [HttpGet]
+    [ProducesResponseType(typeof(List<Project_Json>), StatusCodes.Status200OK)]
+    [ProducesDefaultResponseType(typeof(Error))]
+    public IActionResult Get()
+    {
+        return Ok(context.GetProjects());
+    }
+
+    //GET: api/Projects/24000
+    [HttpGet("{projectNo}")]
+    [ProducesResponseType(typeof(List<Project_Json>), StatusCodes.Status200OK)]
+    [ProducesDefaultResponseType(typeof(Error))]
+    public IActionResult Get(int projectNo)
+    {
+        return Ok(context
+            .GetProjects()
+            .Where(x => x.ProjectNo == projectNo));
+    }
+
+    //POST: api/Projects
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesDefaultResponseType(typeof(Error))]
+    public void Post(NewProject_Json project)
+    {
+        context.PostProject(project);
+    }
+
+    //PUT: api/Projects
+    [HttpPut]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesDefaultResponseType(typeof(Error))]
+    public void Update(Project_Json project)
+    {
+        context.PutProject(project);
+    }
+
+    #endregion
+
+}
+
+[ApiKeyAuthFilter]
+public class ProjectsV2Controller : BaseController
+{
+    #region Constructors
+
+    public ProjectsV2Controller(AtlaswayInternalDbContext dbContext) : base(dbContext) { }
 
     #endregion
 
