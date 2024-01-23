379e5ae362a67fcc639e47a4a834dd2cff84ef79
Micah Bonzet <micah@farsoft.co.za>
2024-01-23 12:20:35 +0200

    Removing appsettings.json

--- AtlaswayInternalAPI/Program.cs
+++ AtlaswayInternalAPI/Program.cs
@@ -36,18 +36,18 @@ public class Program
 
         builder.Services.AddEndpointsApiExplorer();
 
-        builder.Services.AddSwaggerGen();
+        //builder.Services.AddSwaggerGen();
 
         builder.Services.AddScoped<ApiKeyAuthFilter>();
 
         WebApplication app = builder.Build();
 
         // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
+        //if (app.Environment.IsDevelopment())
+        //{
+        //    app.UseSwagger();
+        //    app.UseSwaggerUI();
+        //}
 
         app.UseRouting();
 
